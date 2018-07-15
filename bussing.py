3ad24151caa2016442bb44d8d2c1c13912ed2414
jablonsk19 <jablonsk19@up.edu>
2018-07-15 09:39:57 -0700

    Introduced a way of running Bussing with command line arguments and added the ability to parse paths for desired files.

---
+++ bussing.py
@@ -0,0 +1,47 @@
+import data
+import os
+import sys
+import argparse
+
+def commandLineRun(args):
+    """Perform the steps necessary to bridge the gaps in Thermo-Calc to model heat treatment optimization
+
+    Example:
+    """
+
+    args = args[1:]
+
+    parser = argparse.ArgumentParser(description="Perform the steps necessary to bridge the gaps in "
+                                                 "Thermo-Calc to model heat treatment optimization.",
+                                     prog='bussing')
+    parser.add_argument("path", help="The path of the folder conaining necessary .exp files.  The folder must"
+                                     "not contain any more .exp files than the ones used to run this tool.")
+    parser.add_argument("main", help="The file (in the directory 'path') that plots total mass fraction liquid against "
+                                     "temperature.")
+    ns = parser.parse_args(args)
+    runMain(ns.path, ns.main)
+
+def runMain(filePath, mainFile):
+    """read in all exp files in specified directory and produce outputs to the user."""
+    dataList = []
+    totalLiq = ""
+    for path in os.listdir(filePath):
+
+        if path == mainFile:
+            totalLiq = filePath + mainFile
+            continue
+
+        path = filePath + path
+        if path.endswith("exp"):
+            dataList.append(data.ExpData(os.path.abspath(path)))
+
+    # TODO replace these print statements with the actual functionality of the program
+    for el in dataList:
+        print(el.element)
+        print(el.wtFrLiq)
+        print(el.temperature)
+
+    print(totalLiq)
+
+if __name__ == '__main__':
+    commandLineRun(sys.argv)
--- data.py
+++ data.py
@@ -35,15 +35,3 @@ class ExpData:
                 line = line.split()
                 self.wtFrLiq.append(float(line[0]))
                 self.temperature.append(float(line[1]))
-
-
-
-
-if __name__ == '__main__':
-    # For now, just read in a single file and spit out the outputs (file not accessable to GitHub, but
-    # this is ok because this is just temporary behavior for testing)
-    data = ExpData("../Homogenize-Solid/Liq-Al.exp")
-    print(data.element)
-    print(data.wtFrLiq)
-    print(data.temperature)
-
