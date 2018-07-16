e605932e04c3abae97bd71972803d7954b245e01
jablonsk19 <jablonsk19@up.edu>
2018-07-15 21:27:55 -0700

    Added data massaging step.

--- bussing.py
+++ bussing.py
@@ -22,27 +22,63 @@ def commandLineRun(args):
     ns = parser.parse_args(args)
     runMain(ns.path, ns.main, ns.homog_dist)
 
+
+
+
+
 def runMain(filePath, mainFile, homog_dist):
     """read in all exp files in specified directory and produce outputs to the user."""
+
+    # ---=== Parse data files ===---
+
     dataList = []
-    totalLiq = ""
+    liqData = ""
     for path in os.listdir(filePath):
 
         if path == mainFile:
-            totalLiq = filePath + mainFile
+            liqData = filePath + mainFile
             continue
 
         path = filePath + path
         if path.endswith("exp"):
             dataList.append(data.ExpData(os.path.abspath(path)))
 
-    # TODO replace these print statements with the actual functionality of the program
-    for el in dataList:
-        print(el.element)
-        print(el.wtFrLiq)
-        print(el.temperature)
+    liqData = data.ExpData(liqData)
 
-    print(totalLiq)
+    # ---=== Massage data ===---
+
+    # delete repeated elements
+    for i in range(len(liqData.wtFrLiq) - 1):
+        try:
+            while liqData.wtFrLiq[i] == liqData.wtFrLiq[i + 1]:
+                del liqData.wtFrLiq[i]
+                del liqData.temperature[i]
+                for elem in dataList:
+                    del elem.wtFrLiq[i]
+                    del elem.temperature[i]
+        except IndexError:
+            break
+    liqData.wtFrLiq = data.normalize(liqData.wtFrLiq)
+    liqData.wtFrLiq = [1 - x for x in liqData.wtFrLiq]
+
+    # ---=== Output data ===---
+
+    # TODO replace these print statements with the actual functionality of the program
+    print("\n\nAFTER\n\n")
+
+    for elem in dataList:
+        print(elem.element)
+        print(elem.wtFrLiq)
+        print(elem.temperature)
+
+    print()
+    print(liqData.element)
+    print(liqData.wtFrLiq)
+    print(liqData.temperature)
+
+
+
+
 
 if __name__ == '__main__':
     commandLineRun(sys.argv)
