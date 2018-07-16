3469980780d9596872b9cd0acbf6d2dd7904720c
jablonsk19 <jablonsk19@up.edu>
2018-07-15 21:40:56 -0700

    Changes to documentation

--- bussing.py
+++ bussing.py
@@ -4,21 +4,26 @@ import sys
 import argparse
 
 def commandLineRun(args):
-    """Perform the steps necessary to bridge the gaps in Thermo-Calc to model heat treatment optimization
+    """Perform a command-line version of Bussing.
 
-    Example:
+    Arguments:
+        args -- all command-line arguments (including the name of the program itself)
     """
 
     args = args[1:]
 
     parser = argparse.ArgumentParser(description="Perform the steps necessary to bridge the gaps in "
-                                                 "Thermo-Calc to model heat treatment optimization.",
+                                     "Thermo-Calc to model heat treatment optimization.  \n\nOutputs a folder with "
+                                     "multiple files.  Matrix.dat contains the distance at which each data point "
+                                     "is calculated.  All other files contain their instantaneous percent solid by "
+                                     "weight relative to the distance given by the SDAS.  ",
                                      prog='bussing')
     parser.add_argument("path", help="The path of the folder conaining necessary .exp files.  The folder must"
                                      "not contain any more .exp files than the ones used to run this tool.")
     parser.add_argument("main", help="The file (in the directory 'path') that plots total mass fraction liquid against "
                                      "temperature.")
     parser.add_argument("homog_dist", help= "The distance over which to homogenize, normally (1/2)*(SDAS)")
+
     ns = parser.parse_args(args)
     runMain(ns.path, ns.main, ns.homog_dist)
 
@@ -27,7 +32,14 @@ def commandLineRun(args):
 
 
 def runMain(filePath, mainFile, homog_dist):
-    """read in all exp files in specified directory and produce outputs to the user."""
+    """read in all exp files in specified directory and produce outputs to the user.
+
+    Arguments:
+        filePath -- The path of the folder conaining necessary .exp files.  The folder must not contain any more
+            .exp files than the ones used to run this tool.
+        mainFile -- The file (in the directory 'path') that plots total mass fraction liquid against temperature
+        homog_dist -- the distance over which to homogenize, usually (1/2)*(SDAS)
+    """
 
     # ---=== Parse data files ===---
 
