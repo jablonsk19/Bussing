06d45fbcefc3b174ac58225d83aa659fe37d0ea3
jablonsk19 <jablonsk19@up.edu>
2019-05-26 15:39:25 -0500

    Added PyQt5 to the search paths for pyinstaller
    It seems that this doesn't really make a difference...

--- Run Pyinstaller.bat
+++ Run Pyinstaller.bat
@@ -1,5 +1,5 @@
 
-pyinstaller.exe --icon="HTA Logo.ico" --windowed --onedir --noconfirm --hidden-import PyQt5.sip hta.py 
+pyinstaller.exe --paths C:\Python36\Lib\site-packages\PyQt5\Qt\bin --icon="HTA Logo.ico" --windowed --onedir --noconfirm --hidden-import PyQt5.sip hta.py 
 
 copy main.qml dist\hta
 copy "HTA Logo.png" dist\hta
