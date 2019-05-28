65c9dd1bff60e3b200f21f0595534495b25c91ec
jablonsk19 <jablonsk19@up.edu>
2019-05-27 20:15:10 -0500

    Included extra files for Pyinstaller

--- Run Pyinstaller.bat
+++ Run Pyinstaller.bat
@@ -3,3 +3,5 @@ pyinstaller.exe --paths C:\Python36\Lib\site-packages\PyQt5\Qt\bin --icon="HTA L
 
 copy main.qml dist\hta
 copy "HTA Logo.png" dist\hta
+copy "C:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\x64\*.dll" dist\hta
+
