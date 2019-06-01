b05deaa4e7ebc4494a5f577cc29c7a27559840b4
jablonsk19 <jablonsk19@up.edu>
2019-06-01 14:01:55 -0500

    Tkinter GUI with improved background

--- Run Pyinstaller.bat
+++ Run Pyinstaller.bat
@@ -1,7 +1,8 @@
 
 pyinstaller.exe --paths C:\Python36\Lib\site-packages\PyQt5\Qt\bin --icon="HTA Logo.ico" --windowed --onedir --noconfirm --hidden-import PyQt5.sip hta.py 
 
-copy main.qml dist\hta
+::copy main.qml dist\hta
 copy "HTA Logo.png" dist\hta
-copy "C:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\x64\*.dll" dist\hta
+copy "Gradient.png" dist\hta
+::copy "C:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\x64\*.dll" dist\hta
 
--- tkinterGui.py
+++ tkinterGui.py
@@ -12,40 +12,44 @@ class MainGui:
         self.workingFolder = None
         self.mainFile = None
         master.title("HTA")
+        
+        tempImg = Image.open("Gradient.png")
+        tempImg = tempImg.resize((600, 638))
+
+        self.gradient = ImageTk.PhotoImage(tempImg)
+        self.bgImage = tk.Label(master, image=self.gradient)
+        self.bgImage.place(x=0, y=0, relwidth=1, relheight=1)
 
         self.img = ImageTk.PhotoImage(Image.open("HTA Logo.png"))
         self.ImgWidget = tk.Label(master, image=self.img)
-        self.ImgWidget.pack()
+        self.ImgWidget.place(x=9, y=9, relwidth=1, relheight=1, width=-18, height=-311)
 
-        self.spacer_1 = tk.Label(master, text="")
-        self.spacer_1.pack()
+        self.inputFrame = tk.Frame(master)
+        self.inputFrame.place(x=50, y=347, relwidth=1, relheight=1, width=-100, height=-360)
 
-        self.SDASFrame = tk.Frame(master)
+        self.SDASFrame = tk.Frame(self.inputFrame)
         self.sdasPrompt = tk.Label(self.SDASFrame, text="SDAS (m)")
         self.sdasPrompt.pack(side=tk.LEFT)
         self.sdasField = tk.Entry(self.SDASFrame, font=default_font)
         self.sdasField.pack()
-        self.SDASFrame.pack()
+        self.SDASFrame.pack(pady=10)
 
-        self.spacer_2 = tk.Label(master, text="")
-        self.spacer_2.pack()
-
-        self.WorkingFolderButton = tk.Button(master, text="Browse for working folder", command=self.chooseWorkingFolder)
+        self.WorkingFolderButton = tk.Button(self.inputFrame, text="Browse for working folder", command=self.chooseWorkingFolder)
         self.WorkingFolderButton.pack()
 
-        self.displayWorkingFolder = tk.Label(master, text="")
+        self.displayWorkingFolder = tk.Label(self.inputFrame, text="")
         self.displayWorkingFolder.pack()
 
-        self.MainFileButton = tk.Button(master, text="Select main file", command=self.chooseMainFile)
+        self.MainFileButton = tk.Button(self.inputFrame, text="Select main file", command=self.chooseMainFile)
         self.MainFileButton.pack()
 
-        self.displayMainFile = tk.Label(master, text="")
+        self.displayMainFile = tk.Label(self.inputFrame, text="")
         self.displayMainFile.pack()
 
-        self.RunButton = tk.Button(master, text="Run computation", command=self.run)
+        self.RunButton = tk.Button(self.inputFrame, text="Run computation", command=self.run)
         self.RunButton.pack()
 
-        self.runResult = tk.Label(master, text="")
+        self.runResult = tk.Label(self.inputFrame, text="")
         self.runResult.pack()
 
     def chooseWorkingFolder(self):
@@ -60,7 +64,7 @@ class MainGui:
         if self.workingFolder is None:
             self.runResult["text"] = "Please select a working folder first."
         else:
-            temp = filedialog.askopenfile(initialdir=self.workingFolder)
+            temp = filedialog.askopenfile(initialdir=self.workingFolder, filetypes=[("DATAPLOT files", "*.exp")])
             if temp is not None and temp != "":
                 self.mainFile = temp.name
                 self.displayMainFile["text"] = "You chose: " + self.mainFile
@@ -93,7 +97,7 @@ root = tk.Tk()
 root.geometry("600x638")
 root.resizable(0, 0)
 default_font = font.nametofont("TkDefaultFont")
-default_font.configure(size=12)
+default_font.configure(size=13)
 
 def runGui():
 
